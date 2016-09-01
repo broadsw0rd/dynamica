8fdb61c2058662682cef72d5d6596ed70db9ebe3
Vladimir Bykov <broadsw.rd@yandex.ru>
2016-09-01 08:45:29 +0300

    Update deps

--- dist/dynamica.js
+++ dist/dynamica.js
@@ -2,137 +2,137 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global.Animation = factory());
-}(this, function () { 'use strict';
+}(this, (function () { 'use strict';
 
-  var classCallCheck = function (instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
+
+function noop() {}
+
+function id(value) {
+  return value;
+}
+
+function indexOf(array, item) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+var Animation = function () {
+  Animation.add = function add(animation) {
+    Animation.instances.push(animation);
+  };
+
+  Animation.remove = function remove(animation) {
+    var idx = indexOf(Animation.instances, animation);
+    if (idx !== -1) {
+      Animation.instances.splice(idx, 1);
     }
   };
 
-  function noop() {}
-
-  function id(value) {
-    return value;
-  }
-
-  function indexOf(array, item) {
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] === item) {
-        return i;
-      }
+  Animation.animate = function animate(time) {
+    var animations = Animation.instances.concat();
+    for (var i = 0, animation; i < animations.length; i++) {
+      animation = animations[i];
+      animation.animate(time);
     }
-    return -1;
-  }
+  };
 
-  var Animation = function () {
-    Animation.add = function add(animation) {
-      Animation.instances.push(animation);
-    };
+  function Animation(options) {
+    classCallCheck(this, Animation);
 
-    Animation.remove = function remove(animation) {
-      var idx = indexOf(Animation.instances, animation);
-      if (idx !== -1) {
-        Animation.instances.splice(idx, 1);
-      }
-    };
+    var _ref = options || {};
 
-    Animation.animate = function animate(time) {
-      var animations = Animation.instances.concat();
-      for (var i = 0, animation; i < animations.length; i++) {
-        animation = animations[i];
-        animation.animate(time);
-      }
-    };
-
-    function Animation(options) {
-      classCallCheck(this, Animation);
-
-      var _ref = options || {};
-
-      var duration = _ref.duration;
-      var handler = _ref.handler;
-      var ease = _ref.ease;
-      var onstart = _ref.onstart;
-      var oncancel = _ref.oncancel;
-      var oncomplete = _ref.oncomplete;
+    var duration = _ref.duration;
+    var handler = _ref.handler;
+    var ease = _ref.ease;
+    var onstart = _ref.onstart;
+    var oncancel = _ref.oncancel;
+    var oncomplete = _ref.oncomplete;
 
 
-      if (isNaN(duration)) {
-        throw Error('`duration` should be defined, check https://github.com/broadsw0rd/dynamica#api');
-      }
-
-      this.startTime = 0;
-
-      this.duration = Number(duration);
-      this.handler = handler || noop;
-      this.ease = ease || id;
-
-      this.onstart = onstart || noop;
-      this.oncancel = oncancel || noop;
-      this.oncomplete = oncomplete || noop;
-
-      this.next = [];
-      this._started = false;
+    if (isNaN(duration)) {
+      throw Error('`duration` should be defined, check https://github.com/broadsw0rd/dynamica#api');
     }
 
-    Animation.prototype.start = function start() {
-      Animation.add(this);
-      this._started = true;
-      this.onstart && this.onstart();
-    };
+    this.startTime = 0;
 
-    Animation.prototype.animate = function animate(time) {
-      this.startTime = this.startTime || time;
-      time = (time - this.startTime) / this.duration;
-      if (time < 1) {
-        this.handler(this.ease(time));
-      } else {
-        this.complete();
-      }
-    };
+    this.duration = Number(duration);
+    this.handler = handler || noop;
+    this.ease = ease || id;
 
-    Animation.prototype.complete = function complete() {
-      this.remove();
-      this.handler(1);
-      this.oncomplete && this.oncomplete();
-      for (var i = 0, next; i < this.next.length; i++) {
-        next = this.next[i];
-        next.start();
-      }
-    };
+    this.onstart = onstart || noop;
+    this.oncancel = oncancel || noop;
+    this.oncomplete = oncomplete || noop;
 
-    Animation.prototype.remove = function remove() {
-      this.startTime = 0;
-      Animation.remove(this);
-      this._started = false;
-    };
+    this.next = [];
+    this._started = false;
+  }
 
-    Animation.prototype.cancel = function cancel() {
-      this.remove();
-      this.oncancel && this.oncancel();
-    };
+  Animation.prototype.start = function start() {
+    Animation.add(this);
+    this._started = true;
+    this.onstart && this.onstart();
+  };
 
-    Animation.prototype.queue = function queue(animation) {
-      this.next.push(animation);
-    };
+  Animation.prototype.animate = function animate(time) {
+    this.startTime = this.startTime || time;
+    time = (time - this.startTime) / this.duration;
+    if (time < 1) {
+      this.handler(this.ease(time));
+    } else {
+      this.complete();
+    }
+  };
 
-    Animation.prototype.dequeue = function dequeue(animation) {
-      var idx = indexOf(this.next, animation);
-      if (idx !== -1) {
-        this.next.splice(idx, 1);
-      }
-    };
+  Animation.prototype.complete = function complete() {
+    this.remove();
+    this.handler(1);
+    this.oncomplete && this.oncomplete();
+    for (var i = 0, next; i < this.next.length; i++) {
+      next = this.next[i];
+      next.start();
+    }
+  };
 
-    Animation.prototype.started = function started() {
-      return this._started;
-    };
+  Animation.prototype.remove = function remove() {
+    this.startTime = 0;
+    Animation.remove(this);
+    this._started = false;
+  };
 
-    return Animation;
-  }();
+  Animation.prototype.cancel = function cancel() {
+    this.remove();
+    this.oncancel && this.oncancel();
+  };
 
-  Animation.instances = [];
+  Animation.prototype.queue = function queue(animation) {
+    this.next.push(animation);
+  };
+
+  Animation.prototype.dequeue = function dequeue(animation) {
+    var idx = indexOf(this.next, animation);
+    if (idx !== -1) {
+      this.next.splice(idx, 1);
+    }
+  };
+
+  Animation.prototype.started = function started() {
+    return this._started;
+  };
 
   return Animation;
+}();
 
-}));
+Animation.instances = [];
+
+return Animation;
+
+})));
